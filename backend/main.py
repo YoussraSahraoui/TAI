eb1afe1427d2cef27c6247e5ac3611750ec6a792
youssra <youssra.sahraoui@ensia.edu.dz>
2026-05-15 18:18:36 +0100

    added dependencies

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173", 
+    "https://timetabling-frontend.onrender.com",  # Your frontend URL
+    "https://timetabling-backend.onrender.com"    # Your backend URL
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
